a67608cea17f8621f610b4eaaef449a7540bfd69
Han Ju Kim <hjkim2902@gmail.com>
2024-01-08 18:26:29 +0900

    init commit

---
+++ src/main/java/next/WebServerLauncher.java
@@ -0,0 +1,25 @@
+package next;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.ServletException;
+import java.io.File;
+
+public class WebServerLauncher {
+    public static void main(String[] args) throws ServletException {
+        String webappDirLocation = "./webapp/";
+        Tomcat tomcat = new Tomcat();
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+        Connector connector = tomcat.getConnector();
+        connector.setURIEncoding("UTF-8");
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File(webappDirLocation).getAbsolutePath());
+        tomcat.getServer().await();
+    }
+}
