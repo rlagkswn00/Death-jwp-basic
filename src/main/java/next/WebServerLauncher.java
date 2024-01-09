b55ccbad9c9e642750011882a6ec295735e27b7c
Han Ju Kim <hjkim2902@gmail.com>
2024-01-09 17:53:51 +0900

    feat : tomcat 실행 코드 작성

--- src/main/java/next/WebServerLauncher.java
+++ src/main/java/next/WebServerLauncher.java
@@ -1,5 +1,7 @@
 package next;
 
+
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 
@@ -7,19 +9,16 @@ import javax.servlet.ServletException;
 import java.io.File;
 
 public class WebServerLauncher {
-    public static void main(String[] args) throws ServletException {
-        String webappDirLocation = "./webapp/";
+    public static void main(String[] args) throws ServletException, LifecycleException {
+        String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
 
-        tomcat.setPort(Integer.valueOf(webPort));
+        tomcat.setPort(8080);
         Connector connector = tomcat.getConnector();
         connector.setURIEncoding("UTF-8");
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        System.out.println("configuring app with basedir: " + new File(webappDirLocation).getAbsolutePath());
+        tomcat.addWebapp("", new File(webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
         tomcat.getServer().await();
     }
 }
