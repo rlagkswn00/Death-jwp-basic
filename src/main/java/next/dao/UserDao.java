f46b5ec767ad2648dba2fbefd255581ac3a7fb66
Han Ju Kim <hjkim2902@gmail.com>
2024-03-06 16:24:01 +0900

    feat : @FunctionalInterface 어노테이션 추가

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -41,18 +41,6 @@ public class UserDao {
 
         String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
 
-        RowMapper<User> rowMapper = new RowMapper<User>() {
-
-            @Override
-            public User mapRow(ResultSet rs) throws SQLException {
-                if (rs.next()) {
-                    return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                            rs.getString("email"));
-                }
-                return null;
-            }
-        };
-
         return jdbcTemplate.queryForObject(sql, (ResultSet rs) -> {
             if (rs.next()) {
                 return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
