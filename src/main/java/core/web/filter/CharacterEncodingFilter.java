e15d3ae3a625fe692caf280c86bc31aa6b05211c
Han Ju Kim <hjkim2902@gmail.com>
2024-03-11 21:53:45 +0900

    feat : Filter 추가

--- src/main/java/core/web/filter/CharacterEncodingFilter.java
+++ src/main/java/core/web/filter/CharacterEncodingFilter.java
@@ -1,5 +1,8 @@
 package core.web.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 import javax.servlet.Filter;
@@ -9,10 +12,12 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
     private static final String DEFAULT_ENCODING = "UTF-8";
+    private static final Logger logger = LoggerFactory.getLogger(ResourceFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -22,9 +27,11 @@ public class CharacterEncodingFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         request.setCharacterEncoding(DEFAULT_ENCODING);
+        logger.info("URI : " + ((HttpServletRequest) request).getRequestURI());
         response.setCharacterEncoding(DEFAULT_ENCODING);
+        logger.info(this.getClass() + ": 필터링 전...");
         chain.doFilter(request, response);
-
+        logger.info(this.getClass() + ": 필터링 후...");
     }
 
     @Override
