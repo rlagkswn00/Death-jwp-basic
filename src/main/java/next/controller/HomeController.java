324aad38792ea9bb1874e493f4d31caf1370fe63
Han Ju Kim <hjkim2902@gmail.com>
2024-03-11 21:54:03 +0900

    feat : DB에서 qna가져오기

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -1,11 +1,19 @@
 package next.controller;
 
+import next.dao.QuestionDao;
+import next.model.Question;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class HomeController implements Controller{
     @Override
     public String execute(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        QuestionDao questionDao = new QuestionDao();
+        List<Question> questions = questionDao.findAll();
+
+        httpServletRequest.setAttribute("questions", questions);
         return "/home.jsp";
     }
 }
