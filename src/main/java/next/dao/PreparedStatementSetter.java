40e014aed10a6e75b4924ed60627b701347e0ed3
Han Ju Kim <hjkim2902@gmail.com>
2024-03-11 21:53:39 +0900

    feat : psc 추가

--- src/main/java/next/dao/PreparedStatementSetter.java
+++
@@ -1,8 +0,0 @@
-package next.dao;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface PreparedStatementSetter {
-    void setValues(PreparedStatement pstmt) throws SQLException;
-}
